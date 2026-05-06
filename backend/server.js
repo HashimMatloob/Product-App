8e968e51e1b62f246ac45a373c86c4cbef79cf9d
HashimMatloob <hashimraja67@gmail.com>
2026-05-07 01:22:42 +0500

    mazeed server settings

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
 const app = express();
-
+app.get('/', (req, res) => {
+  res.send('server is ready12');
+});
 app.listen(5000, () => {
   console.log('Server is running on port http://localhost:5000');
 });
